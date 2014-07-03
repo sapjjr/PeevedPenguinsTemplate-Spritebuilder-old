d1989be7b453242d9fe22d5b9208d27f44ab169d
andrewp <andrew@andrewMac-3.local>
2014-07-03 20:30:40 +0100

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Penguin.h
@@ -0,0 +1,13 @@
+//
+//  Penguin.h
+//  PeevedPenguins
+//
+//  Created by andrew on 03/07/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Penguin : CCSprite
+
+@end
---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,22 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by andrew on 03/07/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+// this is just standard initialiser code
+- (id)init {
+    self = [super init];
+    if (self ) {
+        CCLOG(@"Seal Created");
+    }
+    return self;
+}
+
+@end

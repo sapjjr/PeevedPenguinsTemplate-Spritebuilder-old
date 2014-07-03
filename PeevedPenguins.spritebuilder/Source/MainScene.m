9fc411cbe0f1d95570092fad4f16c82e04a44f5a
andrewp <andrew@andrewMac-3.local>
2014-07-03 21:43:46 +0100

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/MainScene.m
+++ PeevedPenguins.spritebuilder/Source/MainScene.m
@@ -13,7 +13,7 @@
 -(void)play {
     
     //CCLOG(@"play button pressed");
-    CCScene *gamePlayScene = [[CCBReader loadAsScene:@"GamePlay"]];
+    CCScene *gamePlayScene = [[CCBReader loadAsScene:@"Gameplay"]];
     [[CCDirector sharedDirector] replaceScene:gamePlayScene];
 }
 @end

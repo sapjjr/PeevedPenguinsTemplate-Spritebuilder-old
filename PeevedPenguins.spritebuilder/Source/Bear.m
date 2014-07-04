283f104dc2d1c52b3758a1d8823c1d73214df69c
andrewp <andrew@andrewMac-3.local>
2014-07-04 17:15:43 +0100

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Bear.m
@@ -0,0 +1,13 @@
+//
+//  Bear.m
+//  PeevedPenguins
+//
+//  Created by andrew on 04/07/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Bear.h"
+
+@implementation Bear
+
+@end
